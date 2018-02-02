6aa89b1de48c84ba3dd7a89f8c00b5319371de1a
Eric Fode <ericfode@gmail.com>
2018-02-02 11:36:00 -0800

    removed curft

--- target/js/compiled/test/huginn/core_test.js
+++
@@ -1,7 +0,0 @@
-// Compiled by ClojureScript 1.9.946 {:target :nodejs}
-goog.provide('huginn.core_test');
-goog.require('cljs.core');
-goog.require('huginn.core');
-goog.require('cljs.test');
-
-//# sourceMappingURL=core_test.js.map
--- target/js/compiled/test/huginn/jwt_test.js
+++
@@ -1,7 +0,0 @@
-// Compiled by ClojureScript 1.9.946 {:target :nodejs}
-goog.provide('huginn.jwt_test');
-goog.require('cljs.core');
-goog.require('huginn.jwt');
-goog.require('cljs.test');
-
-//# sourceMappingURL=jwt_test.js.map
