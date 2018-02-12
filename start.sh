15859ba8a30777d2341dc725484a7f3eeaa95da7
Eric Fode <ericfode@gmail.com>
2018-02-12 04:02:01 -0800

    added some debugging to the start file

--- start.sh
+++ start.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
+echo "starting resin-wifi-connect"
 # Start resin-wifi-connect
 export DBUS_SYSTEM_BUS_ADDRESS=unix:path=/host/run/dbus/system_bus_socket
 /usr/src/app/resin-wifi-connect --clear=false
 
+echo "starting huggin"
 shadow-cljs server dev
