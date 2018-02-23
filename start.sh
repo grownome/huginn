9344a3acd71c50a38e52ae691ed650b2ec2fc978
Eric Fode <ericfode@gmail.com>
2018-02-23 08:23:43 -0800

    updated wifi connect to only run if there is no wifi, and to use the right path

--- start.sh
+++ start.sh
@@ -3,7 +3,14 @@
 echo "starting resin-wifi-connect"
 # Start resin-wifi-connect
 export DBUS_SYSTEM_BUS_ADDRESS=unix:path=/host/run/dbus/system_bus_socket
-/usr/src/app/resin-wifi-connect --clear=false
+iwgetid -r
+
+if [ $? -eq 0 ]; then
+    printf 'Skipping WiFi Connect\n'
+else
+    printf 'Starting WiFi Connect\n'
+    /usr/src/app/wifi-connect
+fi
 
 echo "starting huggin"
 npm start
