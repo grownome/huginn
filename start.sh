d29e86b20c35c3923f32806787280609d0105fa8
Eric Fode <ericfode@gmail.com>
2018-02-23 09:11:53 -0800

    ping google

--- start.sh
+++ start.sh
@@ -3,7 +3,8 @@
 echo "starting resin-wifi-connect"
 # Start resin-wifi-connect
 export DBUS_SYSTEM_BUS_ADDRESS=unix:path=/host/run/dbus/system_bus_socket
-nmcli -t g | grep full
+
+wget --spider http://google.com 2>&1
 
 if [ $? -eq 0 ]; then
     printf 'Skipping WiFi Connect\n'
