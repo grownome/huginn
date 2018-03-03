d19a8f45ad6e5c9ca95ceaf07d38dbf421171395
Eric Fode <ericfode@gmail.com>
2018-03-03 10:48:22 -0800

    make it much simpler for wifi connect to be skipped no google check just check to make sure it has a gateway

--- start.sh
+++ start.sh
@@ -4,7 +4,7 @@ echo "starting resin-wifi-connect"
 # Start resin-wifi-connect
 export DBUS_SYSTEM_BUS_ADDRESS=unix:path=/host/run/dbus/system_bus_socket
 
-wget --spider http://google.com 2>&1
+ip route | grep default
 
 if [ $? -eq 0 ]; then
     printf 'Skipping WiFi Connect\n'
